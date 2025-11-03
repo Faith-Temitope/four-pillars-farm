3e9838c73db08d5860a50cbd9675fff0490ba8c7
Gbejegbe Eugene <eugenegbejegbe@gmail.com>
2025-11-03 18:18:59 +0100

    Edited the header and footer

--- Java/services.js
+++ Java/services.js
@@ -1,3 +1,21 @@
+
+// Fixed Header on Scroll
+window.addEventListener("scroll", () => {
+  const hero = document.querySelector(".hero");
+  const fixedHeader = document.querySelector(".fixedHeader");
+
+  if (hero && window.scrollY > hero.offsetHeight - 500) {
+    fixedHeader.classList.add("show");
+  } else {
+    fixedHeader.classList.remove("show");
+  }
+});
+
+
+
+
+
+
 window.onscroll = function() {
   const btn = document.getElementById("scrollTopBtn");
   btn.style.display = document.documentElement.scrollTop > 300 ? "block" : "none";
